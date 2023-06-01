b654d97229657eb878ecf7cd6e95b16928d37095
Moritz Prünster <internship.project@coolorange.com>
2023-06-01 16:25:30 +0200

    SAT and CreateDXF Updated. DXF is not tested

--- Create DXF from DWG/Jobs/Sample.CreateDXF.ps1
+++ Create DXF from DWG/Jobs/Sample.CreateDXF.ps1
@@ -9,7 +9,65 @@
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  #
 #=============================================================================#
 
-$workingDirectory = "C:\Temp\$($file._Name)"
+# To include the Revision of the main file in the dxf name set Yes, otherwise No
+$dxfFileNameWithRevision = $true
+
+# The character used to separate file name and Revision label in the dxf name such as hyphen (-) or underscore (_)
+$dxfFileNameRevisionSeparator = "_"
+
+# To include the file extension of the main file in the dxf name set Yes, otherwise No
+$dxfFileNameWithExtension = $true
+
+# To add the dxf to Vault set Yes, to keep it out set No
+$addDXFToVault = $true
+
+# To attach the dxf to the main file set Yes, otherwise No
+$attachDCFToVaultFile = $true
+
+# Specify a Vault folder in which the dxf should be stored (e.g. $/Designs/PDF), or leave the setting empty to store the PDF next to the main file
+$dxfVaultFolder = ""
+
+# Specify a network share into which the PDF should be copied (e.g. \\SERVERNAME\Share\Public\PDFs\)
+$dxfNetworkFolder = ""
+
+# To enable faster opening of released Inventor drawings without downloading and opening their model files set Yes, otherwise No
+$openReleasedDrawingsFast = $true
+
+#endregion
+
+
+$dxfFileName = [System.IO.Path]::GetFileNameWithoutExtension($file._Name)
+if ($dxfFileNameWithRevision) {
+    $dxfFileName += $dxfFileNameRevisionSeparator + $file._Revision
+}
+if ($dxfFileNameWithExtension) {
+    $dxfFileName += "." + $file._Extension
+}
+$dxfFileName += ".dxf"
+
+if ([string]::IsNullOrWhiteSpace($dxfVaultFolder)) {
+    $dxfVaultFolder = $file._FolderPath
+}
+
+Write-Host "Starting job 'Create dxf as visualization attachment' for file '$($file._Name)' ..."
+
+
+if( @("dwg") -notcontains $file._Extension ) {
+    Write-Host "Files with extension: '$($file._Extension)' are not supported"
+    return
+}
+if (-not $adddxfToVault -and -not $dxfNetworkFolder) {
+    throw("No output for the dxf is defined in ps1 file!")
+}
+if ($dxfNetworkFolder -and -not (Test-Path $pdfNetworkFolder)) {
+    throw("The network folder '$dxfNetworkFolder' does not exist! Correct dxfNetworkFolder in ps1 file!")
+}
+if(!(Test-Path "$workingDirectory")){
+	New-Item -Path "$workingDirectory" -ItemType Directory | Out-Null
+}
+
+
+
 $Title = $($file.Name).Substring(0,$($file.Name).LastIndexOf("."))
 $localPNGfileLocation = "$workingDirectory\$($Title).dxf"
 $vaultPNGfileLocation = $file._EntityPath +"/"+ (split-path -Leaf $localPNGfileLocation)
@@ -17,13 +75,8 @@ $fastOpen = $file._Extension  -eq "dwg" -and $file._ReleasedRevision
 
 Write-Host "Starting job '$($job.Name)' for file '$($file._Name)' ..."
 
-if( @("dwg") -notcontains $file._Extension ) {
-    Write-Host "Files with extension: '$($file._Extension)' are not supported"
-    return
-}
-if(!(Test-Path "$workingDirectory")){
-	New-Item -Path "$workingDirectory" -ItemType Directory | Out-Null
-}
+
+
 # Not Delete Break-space
 $text = "DXFOUT
 
@@ -35,6 +88,67 @@ $accoreconsolepath = Resolve-Path -Path "C:\Program Files\Autodesk\*\accoreconso
 & $accoreconsolepath[0]  /i "$workingDirectory\$($file._Name)" /s "$workingDirectory\script.scr"
 $DWGfile = Add-VaultFile -From $localPNGfileLocation -To $vaultPNGfileLocation -FileClassification DesignVisualization -Hidden $false
 $file = Update-VaultFile -File $file._FullPath -AddAttachments @($DWGfile._FullPath)
+$fastOpen = $openReleasedDrawingsFast -and $file._ReleasedRevision
+$file = (Save-VaultFile -File $file._FullPath -DownloadDirectory $workingDirectory -ExcludeChildren:$fastOpen -ExcludeLibraryContents:$fastOpen)[0]
+$openResult = Open-Document -LocalFile $file.LocalPath -Options @{ FastOpen = $fastOpen }
+if (-not $openResult) {
+    throw("Failed to open document $($file.LocalPath)! Reason: $($openResult.Error.Message)")
+}
+if (-not $exportResult) {
+    throw("Failed to export document $($file.LocalPath) to $localPDFfileLocation! Reason: $($exportResult.Error.Message)")
+}
+if (-not $closeResult) {
+    throw("Failed to close document $($file.LocalPath)! Reason: $($closeResult.Error.Message))")
+}
+if ($ErrorCopyPDFToNetworkFolder) {
+    throw("Failed to copy PDF file to network folder '$staNetworkFolder'! Reason: $($ErrorCopyPDFToNetworkFolder)")
+}
 
-Clean-Up -folder $workingDirectory
-Write-Host "Completed job '$($job.Name)'"
+Write-Host "Completed job 'Create dxf as visualization attachment'"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
